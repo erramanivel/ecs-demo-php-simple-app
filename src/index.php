5a93dc2feda32673b035c37baed695cf95dbac87
erramanivelasco <ervelasco@ipn.mx>
2019-07-16 21:51:29 -0500

    new commit

--- src/index.php
+++ src/index.php
@@ -16,7 +16,7 @@
         <div class="container">
             <div class="hero-unit">
                 <h1>Aplicacion PHP con CI/CD</h1>
-                <h2>GRACIAS TOTALES</h2>                 
+                <h2>GRACIAS TOTALES Er-ramani Velasco</h2>                 
                 <p>Tu aplicacion PHP esta siendo ejecutada usando GitHub, CodePipeline, CodeBuild, CodeDeploy, ECR y ECS-Fargate.</p>
                 <p>Meetup Julio 2019</p>
                 <br>
